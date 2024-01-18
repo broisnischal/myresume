710122273842979484ccbe0298ee2053eb997086
broisnischal <neeswebservice@gmail.com>
2024-01-18 23:40:46 +0545

    navigation added

--- app/routes/_dashboard/route.tsx
+++ app/routes/_dashboard/route.tsx
@@ -1,6 +1,15 @@
 import { retriveUser } from "@/utils/auth.utils.server";
 import { LoaderFunctionArgs } from "@remix-run/node";
-import { Outlet, useLoaderData } from "@remix-run/react";
+import { NavLink, Outlet, useLoaderData } from "@remix-run/react";
+
+import navigation from "./navigation.json";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const user = retriveUser(request);
@@ -13,9 +22,53 @@ export default function Dashboard() {
 
   console.log(data);
 
+  const showNotification = () => {
+    // Code to request notification permission and show a notification
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        new Notification("Bruh you clicked notification!", {
+          body: "Hi i am broisnees and i am learning to build the web better.",
+          icon: "/favicon.ico",
+          renotify: true,
+          requireInteraction: true,
+          tag: "hello-notification",
+          image: "/favicon.ico",
+          data: {
+            url: "https://example.com",
+            date: new Date().toLocaleString(),
+          },
+          vibrate: [100, 50, 100],
+        });
+      }
+    });
+  };
+
   return (
     <>
-      <h1>Your Profile</h1>
+      {/* <h1>My Resume</h1> */}
+
+      <div className="flex w-full gap-4 py-5 items-center justify-center">
+        {navigation.map((item, index) => {
+          return (
+            <div key={index}>
+              <NavLink
+                className={({ isActive }) => {
+                  const className =
+                    "px-5 grid place-content-center min-h-[30px] py-1 text-[14px] capitalize";
+                  return isActive
+                    ? "bg-[#3d3d3d61] text-white  rounded-full  " + className
+                    : "text-gray-500 " + className;
+                }}
+                to={`/resume/${item.link}`}
+              >
+                {item.name}
+              </NavLink>
+            </div>
+          );
+        })}
+      </div>
+
+      <button onClick={showNotification}>Show Notification</button>
 
       <Outlet />
     </>
