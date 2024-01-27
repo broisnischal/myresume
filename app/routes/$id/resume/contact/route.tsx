796ad3fc7551d2b70dc5d48e2cad1769be0761b2
broisnischal <neeswebservice@gmail.com>
2024-01-27 17:53:13 +0545

    feat: added feature

--- app/routes/$id/resume/contact/route.tsx
+++ app/routes/$id/resume/contact/route.tsx
@@ -4,7 +4,7 @@ import { ValidatedForm } from "remix-validated-form";
 export default function LinkPage() {
   return (
     <div className="container ">
-      <h2 className="text-3xl font-bold">Contact Information</h2>
+      <h2 className="text-3xl font-bold mb-5">Contact Information</h2>
 
       <ValidatedForm className="flex flex-col gap-3 w-1/2">
         <FieldInput
